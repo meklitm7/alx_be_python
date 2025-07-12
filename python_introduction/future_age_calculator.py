4171c0f41fb92f4ec73f59752085de04baff8c21
MEKLIT MULUGETA TAFFA <meklitmulugeta71@gmail.com>
2025-07-12 16:09:41 +0300

    Update future_age_calculator.py

---
+++ python_introduction/future_age_calculator.py
@@ -0,0 +1,7 @@
+current_age = int(input("How old are you?"))
+current_year = 2023
+future_year = 2050 - current_year
+
+future_age = current_age + future_year
+
+print("in 2050, you will be", future_age, "years old.")
