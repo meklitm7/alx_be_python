827b36734afda3b347f31267dea097d9ca4a0ab8
MEKLIT MULUGETA TAFFA <meklitmulugeta71@gmail.com>
2025-07-13 13:49:52 +0300

    Update finance_calculator.py

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -1,16 +1,9 @@
-# Welcome and Income Input
-username = input("What's your name?: ")
-print(f"Welcome to {username}'s Budget Manager!")
-salary = float(input("What's your monthly income (salary)?: "))
-business = float(input("What's your monthly business income?: "))
-total_income = salary + business
-print(f"Total Monthly Income: ${total_income:.2f}")
+monthly_income = int(input("Enter your monthly income: "))
+monthly_expenses = int(input("Enter your total monthly expenses: "))
 
-# Expenses and Savings Calculation
-transport = float(input("What's your transportation expenses for the month?: "))
-food = float(input("Food expenses for the month?: "))
-total_expenses = transport + food
-print(f"Total Expenses for the Month: ${total_expenses:.2f}")
+monthly_savings = monthly_income - monthly_expenses
 
-monthly_savings = total_income - total_expenses
-print(f"Total Monthly Savings: ${monthly_savings:.2f}")
+annual_savings = monthly_savings * 12 + (monthly_savings * 12 * 0.05)
+
+print(f"Your monthly savings are ${monthly_savings}."  )
+print(f"Projected savings after one year, with interest, is: ${int(annual_savings)}."  )
